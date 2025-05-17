d50a6874ab22da85e2f80f20d961690551e12631
Alexander Sagalovskiy <spikesagal@gmail.com>
2025-05-16 22:39:59 -0600

    routing and pagination

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,21 @@
-import 'src/assets/styles/App.less';
+import { Routes, Route } from 'react-router';
+
+import Layout from 'src/components/Layout';
+import Pokemons from 'src/components/Pokemons';
+import Abilities from 'src/components/Abilities';
+import NotFound from 'src/components/NotFound';
 
 const App = (): React.ReactNode => {
-  return <div>{/* POKEMONS */}</div>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<Pokemons />} />
+        <Route path='abilities/' element={<NotFound />} />
+        <Route path='abilities/:pokemonName' element={<Abilities />} />
+        <Route path='*' element={<NotFound />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
