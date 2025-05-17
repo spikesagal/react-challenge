379770db344abe3340edef99490eb06dd3f4499c
Alexander Sagalovskiy <spikesagal@gmail.com>
2025-05-17 03:45:11 -0600

    added unit test for useAbilities

--- src/hooks/PokedexConnector.test.ts
+++ src/hooks/PokedexConnector.test.ts
@@ -1,23 +1,21 @@
-import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { describe, it, expect, vi } from 'vitest';
 import { renderHook, waitFor } from '@testing-library/react';
 
 import { createWrapper } from 'src/utils/test-utils';
 import api from 'src/api/PokemonAPI';
-import { usePokemons } from './PokedexConnector';
+import { usePokemons, useAbilities } from './PokedexConnector';
 
 vi.mock('src/api/PokemonAPI', () => ({
   default: {
-    listPokemons: vi.fn()
+    listPokemons: vi.fn(),
+    getPokemonByName: vi.fn(),
+    getAbilityByName: vi.fn()
   }
 }));
 
 describe('PokedexConnector', () => {
   describe('usePokemons', () => {
-    beforeEach(() => {
-      // queryClient = new QueryClient();
-    });
-
-    it('should return a list of 5 pokemon per requested page, along with the total page count', async () => {
+    it('should return a list of 5 pokemon names, along with the total page count', async () => {
       const mockResponse = {
         'count': 1302,
         'next': 'https://pokeapi.co/api/v2/pokemon/?offset=5&limit=5',
@@ -64,6 +62,76 @@ describe('PokedexConnector', () => {
           'charmeleon'
         ]
       });
+    });
+  });
+  describe('useAbilities', () => {
+    it('should return a list of abilities names for a pokemon', async () => {
+      (api.getPokemonByName as any).mockResolvedValue({
+        'abilities': [
+          {
+            'ability': {
+              'name': 'overgrow',
+              'url': 'https://pokeapi.co/api/v2/ability/65/'
+            },
+            'is_hidden': false,
+            'slot': 1
+          },
+          {
+            'ability': {
+              'name': 'chlorophyll',
+              'url': 'https://pokeapi.co/api/v2/ability/34/'
+            },
+            'is_hidden': true,
+            'slot': 3
+          }
+        ]
+      });
+
+      (api.getAbilityByName as any).mockResolvedValueOnce({
+        name: 'overgrow',
+        effect_entries: [
+          {
+            effect:
+              'When this Pokémon has 1/3 or less of its HP remaining, its grass-type moves inflict 1.5× as much regular damage.',
+            language: { name: 'en' }
+          }
+        ]
+      });
+
+      (api.getAbilityByName as any).mockResolvedValueOnce({
+        name: 'chlorophyll',
+        effect_entries: [
+          {
+            effect:
+              "This Pokémon's Speed is doubled during strong sunlight. This bonus does not count as a stat modifier.",
+            language: { name: 'en' }
+          }
+        ]
+      });
+
+      const { result } = renderHook(() => useAbilities('bulbasaur'), {
+        wrapper: createWrapper()
+      });
+
+      // Because this is a chained query, first one will always
+      // return undefined, so wait for the second one
+      await waitFor(() => expect(result.current.length).toBe(2));
+      await waitFor(() =>
+        expect(result.current.every((q) => q.isSuccess)).toBe(true)
+      );
+      const abilities = result.current.map((q) => q.data);
+      expect(abilities).toEqual([
+        {
+          'effect':
+            'When this Pokémon has 1/3 or less of its HP remaining, its grass-type moves inflict 1.5× as much regular damage.',
+          'name': 'overgrow'
+        },
+        {
+          'effect':
+            "This Pokémon's Speed is doubled during strong sunlight. This bonus does not count as a stat modifier.",
+          'name': 'chlorophyll'
+        }
+      ]);
     });
   });
 });
